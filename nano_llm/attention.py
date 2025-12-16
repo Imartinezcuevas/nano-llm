84a91d16fcdfa983d3ab710b57d2bc421a1560c0
Iván Martínez Cuevas <imartinezcuevas@gmail.com>
2025-12-16 11:26:45 +0100

    feat(attention): optional padding and dropout

--- nano_llm/attention.py
+++ nano_llm/attention.py
@@ -1,9 +1,14 @@
+'''
+
+'''
+
 import torch
 import torch.nn as nn
 import math
 
+
 class MultiHeadAttention(nn.Module):
-    def __init__(self, d_model:int, num_heads:int):
+    def __init__(self, d_model:int, num_heads:int, dropout:float=0.0):
         super().__init__()
 
         if d_model % num_heads != 0:
@@ -16,6 +21,8 @@ class MultiHeadAttention(nn.Module):
         self.q_proj = nn.Linear(d_model, d_model)
         self.k_proj = nn.Linear(d_model, d_model)
         self.v_proj = nn.Linear(d_model, d_model)
+
+        self.attn_dropout = nn.Dropout(dropout)
 
         self.out_proj = nn.Linear(d_model, d_model)
 
@@ -32,13 +39,19 @@ class MultiHeadAttention(nn.Module):
 
         return q, k, v
     
-    def forward(self, x: torch.Tensor):
+    def forward(self, x: torch.Tensor, padding_mask:torch.Tensor=None):
         B, T, D = x.shape
 
         q, k, v = self.project_qkv(x)
 
-        mask = torch.tril(torch.ones(T, T, device=x.device))
-        attn_out = scaled_dot_product_attention(q, k, v, mask)
+        mask = torch.tril(torch.ones(T, T, device=x.device)).unsqueeze(0).unsqueeze(0)
+        if padding_mask is not None:
+            padding_mask = padding_mask[:, None, None, :]
+            mask = mask.bool() & padding_mask
+        else:
+            mask = mask.bool()
+
+        attn_out = self.scaled_dot_product_attention(q, k, v, mask)
 
         #concatenate heads: (B, num_heads, T, d_head) -> (B, T, d_model)
         B, num_heads, T, d_head = attn_out.shape
@@ -49,16 +62,17 @@ class MultiHeadAttention(nn.Module):
 
         return out
 
-def scaled_dot_product_attention(q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, mask: torch.Tensor):
-    d_k = q.size(-1)
+    def scaled_dot_product_attention(self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, mask: torch.Tensor):
+        d_k = q.size(-1)
 
-    scores = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(d_k)
+        scores = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(d_k)
 
-    # apply mask if provided
-    if mask is not None:
-        scores = scores.masked_fill(mask == 0, float('-inf'))
+        # apply mask if provided
+        if mask is not None:
+            scores = scores.masked_fill(mask == 0, float('-inf'))
 
-    attn = torch.softmax(scores, dim=-1)
-    out = torch.matmul(attn, v)
+        attn = torch.softmax(scores, dim=-1)
+        attn = self.attn_dropout(attn)
+        out = torch.matmul(attn, v)
 
-    return out
+        return out
